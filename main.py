a74b7bc7b296009720c1695f7936737c7ae51efe
federicoVaona <federico.vaona@mail.polimi.it>
2024-08-09 17:55:17 +0200

    First visualizations try on streamlit

--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import visualization_functions as vis
 import streamlit as st
 import matplotlib.pyplot as plt
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
 original_videogames_df = pd.read_csv('Dataset/metacritic_18.07.2021_csv.csv')
-# I cleaned the dataframe on colab
+# I did the complete EDA part and cleaned the dataframe on colab notebook uploaded on github
 cleaned_videogames_df = pd.read_csv('Dataset/clean_dataset.csv')
 
 # Initializing web page
@@ -27,7 +30,7 @@ st.markdown("""
 
 
 #####################
-# EDA
+# some EDA
 #####################
 st.sidebar.write('Choose which dataset yuo want to see:')
 if st.sidebar.checkbox('original dataset'):
@@ -42,7 +45,34 @@ if st.sidebar.checkbox('dataset after the data cleaning'):
         st.write('Numerical value after the cleaning:')
         st.write(cleaned_videogames_df.describe().T)
 
+#####################
+# Visualization Section
+#####################
 
-#####################
-# Show some interesting plot
-#####################
+st.write("## Visualizations")
+
+selection = st.selectbox('Select if you want to see the **number of game per platform** or **number of game per genre**:', [" ","genre","platform"])
+if selection == "genre":
+        # number of games for each platforms
+        general_counts = cleaned_videogames_df[selection].value_counts()
+        vis.plot_bar((12, 8), general_counts, col='lightgreen', title='Number of Games per ' + selection, x_lab = selection, y_lab='Number of Games', rot=90)
+elif selection == "platform":
+        general_counts = cleaned_videogames_df[selection].value_counts()
+        vis.plot_bar((12, 8), general_counts, col = 'lightblue', title = 'Number of Games per ' + selection, x_lab = selection, y_lab = 'Number of Games', rot=90)
+
+
+
+# Comparison between Metascore and Userscore
+st.write("### Comparison between Metascore and Userscore")
+
+st.write("### Comparison of the two distribution")
+
+col1, col2 = st.columns(2)
+with col1:
+        vis.plot_histogram(cleaned_videogames_df, 'metascore', 'Metascore Distribution', color='blue')
+with col2:
+        vis.plot_histogram(cleaned_videogames_df, 'userscore', 'Userscore Distribution', color='orange')
+
+st.write("We can see that both the score metric follow a Normal Distribution.")
+
+
--- visualization_functions.py
+++ visualization_functions.py
@@ -1,17 +1,36 @@
 import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Function to visualize simple text to web page
-def write(text):
-    st.write(text)
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# Function to visualize dataframe to web page
-def write_df(df):
-    st.dataframe(df)
+# Function to plot a bar chart
+def plot_bar(fig_size, datas, col = 'blue', title = '', x_lab = '', y_lab = '', rot=0 ):
+  plt.figure(figsize = fig_size)
+  bars = plt.bar(datas.index, datas.values, color=col)
+  plt.title(title)
+  plt.xlabel(x_lab)
+  plt.ylabel(y_lab)
+  plt.xticks(rotation= rot)
+  # Annotazione dei valori sopra le barre
+  for bar in bars:
+      yval = bar.get_height()
+      plt.text(bar.get_x() + bar.get_width()/2, yval, int(yval), ha='center', va='bottom', fontsize=10)
+  st.pyplot()
 
-# Function to create a title to web page
-def write_title(text):
-    st.title(text)
+def plot_histogram(data, column, title, bins=50, color='blue'):
+    fig, ax = plt.subplots()
+    sns.histplot(data[column].dropna(), bins=bins, ax=ax, color=color)
+    ax.set_title(title)
+    ax.set_xlabel(column)
+    st.pyplot(fig)
 
-# Function to create a title to web page
-def write_header(text):
-    st.header(text)
+def plot_correlation_heatmap(data, title):
+    fig, ax = plt.subplots(figsize=(12, 10))
+    corr_matrix = data.corr(numeric_only=True)
+    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', ax=ax)
+    ax.set_title(title)
+    st.pyplot(fig)
+
+
+
